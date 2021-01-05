528c5e1a0269a5c7685c0a4ea8282c7c9ccbe76c
happy_sadan <tristancottam.1@gmail.com>
2021-01-05 11:19:47 +0100

    style(norm): Fix printf defines comment

--- cub3d.h
+++ cub3d.h
@@ -6,20 +6,20 @@
 /*   By: trcottam <trcottam@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 21:47:22 by trcottam          #+#    #+#             */
-/*   Updated: 2021/01/05 11:17:28 by trcottam         ###   ########.fr       */
+/*   Updated: 2021/01/05 11:19:42 by trcottam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-
-// THE FOLLOWING DEFINES MUST BE REMOVED AFTER HAVING ADDED ft_printf() AND
-// ft_fprintf() TO libft
+/*
+** THE FOLLOWING DEFINES MUST BE REMOVED AFTER HAVING ADDED ft_printf() AND
+** ft_fprintf() TO libft
+*/
 
 # define ft_printf printf
 # define ft_fprintf fprintf
-
 
 # include "libft.h"
 
